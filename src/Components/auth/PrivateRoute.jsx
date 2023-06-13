5f651f380226ab51b9d2e3e978de4ce7d6bd9c3e
SharukhGV <sharukhvelupillai@pursuit.org>
2023-06-13 02:25:47 -0400

    private route potential auth fix

--- src/Components/auth/PrivateRoute.jsx
+++ src/Components/auth/PrivateRoute.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import firebase from 'firebase/compat/app';
+// import firebase from 'firebase/compat/app';
 import 'firebase/auth';
+import { useEffect,useState } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../../firebase';
 // import FirebaseAuthContext from './FirebaseAuthContext';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const currentUser = firebase.auth.currentUser;
+    const [userShow, setUserShow]=useState(true)
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          setUserShow(true);
+        //   setUserShowNAME(`Welcome Back ${user.email}`);
+          // ...
+          console.log("uid", uid)
+
+        } else {
+          // User is signed out
+          // ...
+          console.log("user is logged out");
+          setUserShow(false);
+        //   setUserShowNAME(`Sweet Dreams! See You Soon!`);
+
+        }
+    })})
+    
+
+
+
+
+
+//   const currentUser = firebase.auth.currentUser;
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        currentUser ? <Component {...props} /> : <Navigate to="/login" />
+        userShow ? <Component {...props} /> : <Navigate to="/login" />
       }
     />
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -59,7 +59,7 @@ const [userShowNAME, setUserShowNAME]=useState('')
     return(
     <div className="homePAge">
     <h1>Welcome to Dream Journal!</h1>
-    <p>Dreams are important from a secular and religious aspect</p>
+    <p>A Dream Log App Focused on Simplicity</p>
     <p style={pixstyle}>August Kekulé figured out the chemical configuration structure of Benzene. He notably says that he came about this discovery through a dream he had of a snake biting its own tail. August Kekulé, the scientist responsible for this contribution to field of organic chemistry has been proven to be correct today through various spectroscopic techniques.</p> 
     <p style={pixstyle}>Everyone has dreams! This application is a digital journal built to keep track of the dreams that you had while sleeping! In the section about Good dreams, bad dreams, or neutral dreams, it is totally up to User discretion to determine whether their dream was good or bad or neutral. An example of a good dream could be you wake up feeling happy and hopeful. An example of a bad dream could be waking up in a state of terror. A neutral dream could be something you dreamed about that happened in the previous day.
           </p>
